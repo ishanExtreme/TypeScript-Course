dea911bde92d191c4c4c4b9f35dedc9cc0a18d77
Extreme <extreme@pop-os.localdomain>
2022-03-19 12:41:58 +0530

    className style fixed

--- src/App.tsx
+++ src/App.tsx
@@ -79,45 +79,13 @@ function App() {
         <div className="flex space-x-2 justify-center">
           <button 
           type="submit" 
-          className="inline-block px-6 py-2.5 
-          bg-blue-600 
-          text-white 
-          font-medium text-xs 
-          leading-tight 
-          uppercase 
-          rounded shadow-md 
-          hover:bg-blue-700 
-          hover:shadow-lg 
-          focus:bg-blue-700 
-          focus:shadow-lg 
-          focus:outline-none 
-          focus:ring-0 
-          active:bg-blue-800 
-          active:shadow-lg 
-          transition 
-          duration-150 ease-in-out">
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
             Submit
           </button>
 
           <button
           onClick={clearCB} 
-          className="inline-block px-6 py-2.5 
-          bg-blue-600 
-          text-white 
-          font-medium text-xs 
-          leading-tight 
-          uppercase 
-          rounded shadow-md 
-          hover:bg-blue-700 
-          hover:shadow-lg 
-          focus:bg-blue-700 
-          focus:shadow-lg 
-          focus:outline-none 
-          focus:ring-0 
-          active:bg-blue-800 
-          active:shadow-lg 
-          transition 
-          duration-150 ease-in-out">
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
             Clear
           </button>
         </div>
--- src/FormField.tsx
+++ src/FormField.tsx
@@ -8,24 +8,7 @@ export default function FormField(props:{label:string, type:string, handleChange
                 type={props.type}
                 onChange={props.handleChangeCB}
                 value={props.value} 
-                className="form-control
-                block
-                w-full
-                px-3
-                py-1.5
-                text-base
-                font-normal
-                text-gray-700
-                bg-white bg-clip-padding
-                border border-solid border-gray-300
-                rounded
-                transition
-                ease-in-out
-                m-0
-                focus:text-gray-700 
-                focus:bg-white 
-                focus:border-blue-600 
-                focus:outline-none"/>
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700  focus:bg-white focus:border-blue-600 focus:outline-none"/>
                 
                 <label htmlFor="floatingInput" className="text-gray-700">
                     {props.label}
@@ -35,23 +18,7 @@ export default function FormField(props:{label:string, type:string, handleChange
                 <div className="flex space-x-2 justify-center">
                     <button
                     onClick={props.handleClickCB} 
-                    className="inline-block px-6 py-2.5 
-                    bg-blue-600 
-                    text-white 
-                    font-medium text-xs 
-                    leading-tight 
-                    uppercase 
-                    rounded shadow-md 
-                    hover:bg-blue-700 
-                    hover:shadow-lg 
-                    focus:bg-blue-700 
-                    focus:shadow-lg 
-                    focus:outline-none 
-                    focus:ring-0 
-                    active:bg-blue-800 
-                    active:shadow-lg 
-                    transition 
-                    duration-150 ease-in-out">
+                    className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                         {props.label === "Add Field"?"Add Field":"Remove"}
                     </button>
                 </div>
