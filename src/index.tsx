8cb8db7275dadf3412e07f11a272acf6ce12bc1b
Extreme <extreme@pop-os.localdomain>
2022-04-17 00:00:05 +0530

    sentry added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+Sentry.init({
+  dsn: "https://7abe599c2cfe4626a373664622405f26@o1207046.ingest.sentry.io/6340486",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
